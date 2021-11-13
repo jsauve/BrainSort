9e3d178f38ec384457090f229a6619c991fc7c89
Joe Sauve <joe@joesauve.com>
2021-11-13 13:56:10 -0600

    Commit initial code

---
+++ BrainSort/MainPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using PropertyChanged;
+
+namespace BrainSort
+{
+    [AddINotifyPropertyChangedInterface]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            Entry.TextChanged += Entry_TextChanged;
+        }
+
+        private void Entry_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            ReversedText = new string(e?.NewTextValue?.ToUpperInvariant()?.Where(char.IsLetterOrDigit)?.Reverse()?.ToArray()) ?? null;
+            SortedText = new string(e?.NewTextValue?.ToUpperInvariant()?.Where(char.IsLetterOrDigit)?.OrderBy(x => x)?.ToArray()) ?? null;
+            ReversedSortedText = new string(e?.NewTextValue?.ToUpperInvariant()?.Where(char.IsLetterOrDigit)?.OrderByDescending(x => x)?.ToArray()) ?? null;
+            CharacterCount = ReversedText?.Count().ToString() ?? null;
+            IsShowingLabels = ReversedText.Any();
+
+            if (ReversedText.Count() % 16 == 0)
+                CountColor = Color.Green;
+            else if (ReversedText.Count() % 4 == 0)
+                CountColor = Color.Orange;
+            else
+                CountColor = Color.Red;
+        }
+
+        public string EntryText { get; set; }
+        public string ReversedText { get; set; }
+        public string SortedText { get; set; }
+        public string ReversedSortedText { get; set; }
+        public string CharacterCount { get; set; }
+        public bool IsShowingLabels { get; set; }
+        public Color CountColor { get; set; } = Color.Red;
+    }
+}
